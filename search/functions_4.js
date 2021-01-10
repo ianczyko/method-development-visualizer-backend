600284658f200a26200b2a12365c01e25161b1ba
ianczyko <ianczyko@users.noreply.github.com>
2021-01-10 22:50:47 +0000

    deploy: 9e89fc687fa56b0522c6212e5270f068cc0ab103

--- search/functions_4.js
+++ search/functions_4.js
@@ -3,5 +3,5 @@ var searchData=
   ['ratenamesimilarity_38',['rateNameSimilarity',['../classNode.html#acc0dca05d7f000608f2fad331fbc1a87',1,'Node']]],
   ['removealias_39',['removeAlias',['../classNode.html#a51f43cb1af074900454a85de4d2ccc0b',1,'Node']]],
   ['removechild_40',['removeChild',['../classNode.html#ae8ddbb9e189f90d0f19ea08f5ba00ec9',1,'Node']]],
-  ['removenode_41',['removeNode',['../classTree.html#add6919bd07e185b2832d9a3010ffad48',1,'Tree']]]
+  ['removenode_41',['removeNode',['../classTree.html#aa2847c1a8659f36e3f4702231f6c8bb3',1,'Tree']]]
 ];
