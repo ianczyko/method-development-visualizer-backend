b87bbfaa847b34bc5077de89a5d7f4819bbb8907
ianczyko <ianczyko@users.noreply.github.com>
2021-01-10 20:24:02 +0000

    deploy: 6e55141014eca5285f5cd1069dd39dd8a751897d

--- search/all_4.js
+++ search/all_4.js
@@ -3,5 +3,5 @@ var searchData=
   ['ratenamesimilarity_13',['rateNameSimilarity',['../classNode.html#acc0dca05d7f000608f2fad331fbc1a87',1,'Node']]],
   ['removealias_14',['removeAlias',['../classNode.html#a51f43cb1af074900454a85de4d2ccc0b',1,'Node']]],
   ['removechild_15',['removeChild',['../classNode.html#ae8ddbb9e189f90d0f19ea08f5ba00ec9',1,'Node']]],
-  ['removenode_16',['removeNode',['../classTree.html#add6919bd07e185b2832d9a3010ffad48',1,'Tree']]]
+  ['removenode_16',['removeNode',['../classTree.html#aa2847c1a8659f36e3f4702231f6c8bb3',1,'Tree']]]
 ];
